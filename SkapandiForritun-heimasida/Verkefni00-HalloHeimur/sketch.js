d36d34e92cb7c6d20fdd53840b253b7e35d15032
solvikarlstefansson <Solvikarl1109@gmail.com>
2021-03-17 14:51:12 +0000

    breyta nafni
    breytti nafni bæði í titli og texta

---
+++ SkapandiForritun-heimasida/Verkefni00-HalloHeimur/sketch.js
@@ -0,0 +1,12 @@
+// Hér kemur kóðinn þinn:
+
+function setup() {
+  createCanvas(400, 400);
+  background(255,200,0);
+  frameRate(4);
+}
+
+function draw() {
+  fill(random(0,255));
+  ellipse(200,200,50,50);
+}
