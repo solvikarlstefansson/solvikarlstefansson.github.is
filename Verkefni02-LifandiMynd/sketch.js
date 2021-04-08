b10a1c8f6124402ab76cc1873554538c9ee36d4a
solvikarlstefansson <Solvikarl1109@gmail.com>
2021-04-08 10:45:46 +0000

    breytti lit í verkefni 2

--- Verkefni02-LifandiMynd/sketch.js
+++ Verkefni02-LifandiMynd/sketch.js
@@ -10,7 +10,7 @@ function draw() {
   strokeWeight(3);
   fill(100,100,100); //sett upp sem(x-hnit,y-hnit,lengd,breydd);
   triangle(0, 600, 300, 400, 600, 600);//fjallið
-  fill(random(100,255),4,4);
+  fill(random(100,250),4,4);
   ellipse(300,430,100,50);//gígurinn
   fill(150,150,150);
   triangle(300,430,100,random(50,300),500,random(50,200));//gos mökkur
