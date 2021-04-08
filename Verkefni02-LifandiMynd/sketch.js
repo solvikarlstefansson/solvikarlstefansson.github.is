7c447a77acceea50be33eab6d2c70b652b060cbc
solvikarlstefansson <Solvikarl1109@gmail.com>
2021-04-08 11:03:32 +0000

    bætti inn texta

--- Verkefni02-LifandiMynd/sketch.js
+++ Verkefni02-LifandiMynd/sketch.js
@@ -5,13 +5,13 @@ function setup() {
   frameRate(4);
 
 }
-
 function draw() {
   strokeWeight(3);
-  fill(100,100,100); //sett upp sem(x-hnit,y-hnit,lengd,breydd);
+  fill(100,100,100); //sett upp sem(x-hnit,y-hnit,lengd,breydd);fill=RGB
   triangle(0, 600, 300, 400, 600, 600);//fjallið
   fill(random(100,250),4,4);
   ellipse(300,430,100,50);//gígurinn
   fill(150,150,150);
   triangle(300,430,100,random(50,300),500,random(50,200));//gos mökkur
+
 }
