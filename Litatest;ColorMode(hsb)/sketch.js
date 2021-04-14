63d191bd48d6050f2e8bbb1e56e79f9c1b89c860
solvikarlstefansson <Solvikarl1109@gmail.com>
2021-04-14 09:29:56 +0000

    Update sketch.js

--- Litatest;ColorMode(hsb)/sketch.js
+++ Litatest;ColorMode(hsb)/sketch.js
@@ -1,7 +1,7 @@
 // Hér kemur kóðinn þinn:
 
 let h = 0;
-var s = 400;
+
 function setup() {
   createCanvas(400, 400);
 
@@ -14,15 +14,5 @@ function draw() {
 h++
 if(h>=360){
   h=0;
-
-  rect(0,0,s,s);
-  s--
-  if(s<=0){
-    s=400;
-  }
-
 }
-
-
-
 }
