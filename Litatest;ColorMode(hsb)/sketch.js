39796a8309f3e0324881a3100933806326248fad
solvikarlstefansson <Solvikarl1109@gmail.com>
2021-04-16 18:17:39 +0000

    allskonar

--- Litatest;ColorMode(hsb)/sketch.js
+++ Litatest;ColorMode(hsb)/sketch.js
@@ -1,18 +1,41 @@
 // Hér kemur kóðinn þinn:
 
-let h = 0;
 
 function setup() {
-  createCanvas(400, 400);
-
+  createCanvas(400,400);
+  frameRate(2);
+  strokeWeight(2);
+  background(0);
   colorMode(HSB);
+  rectMode(CENTER);
 }
 
-function draw() {
-  background(h, 100, 200);
+function draw(){
 
-h++
-if(h>=360){
-  h=0;
+  for(var x = 0; x < width ; x = x + 1){
+    for(var y = 0; y < height ; y = y+1 ){
+      fill(random(0,360),75,100);
+    ellipse(10+x*20,10+y*20,15,15);
+  }
 }
+    fill(0,360,0);
+    ellipse(200,150,100,100);
+    ellipse(150,100,80,80);
+    ellipse(250,100,80,80);
+    fill(36, 37, 100);
+    noStroke();
+    ellipse(180,160,40,80);
+    ellipse(220,160,40,80);
+    ellipse(200,180,90,50);
+    fill(0,0,360);
+    ellipse(180,150,20,40);
+    ellipse(220,150,20,40);
+    fill(0,360,0);
+    ellipse(180,160,15,20);
+    ellipse(220,160,15,20);
+    ellipse(200,170,20,15);
+    stroke(0,360,0);
+    strokeWeight(4);
+    line(185,190,215,190);
+
 }
--- Verkefni05-Lykjulist/sketch.js
+++ Verkefni05-Lykjulist/sketch.js
@@ -1,19 +1,47 @@
+var l = 40;
+var x = 400;
+var y =200;
+var f = 100;
 
 function setup() {
   createCanvas(400,400);
-  frameRate(2);
+  frameRate(100);
   strokeWeight(2);
-    background(0,0,0);
+  background(0,0,250);
   colorMode(HSB);
   rectMode(CENTER);
 }
 
 function draw(){
-    for(var i = 10; i<hight;i=i+10){
-      for(var x = 10; x<width; x=x+5){
-      rect(random(0,400),random(0,400),x,i);
-    }
-    }
+  fill(0,100,0);
+  rect(x,200,x,400)
+  fill(54,100,100);
+  ellipse(x,100,80,80);
+  x--
+  if(x<=0){
+    x=400;
+  }
+
+  fill(24,100,37);
+  rect(150,250,50,150);
+  frameRate(4);
+  fill(l,100,100);
+  l++
+  if(l>=100){
+    l=40;
+  }
+  ellipse(150,150,150,100);
+  rect(200,400,400,200);
 
 
+  for(var r = 0; r < width ; r = r + 1){
+    for(var f = 0; f < height+300 ; f = f+1 ){
+      fill(260,random(0,100),100);
+    ellipse(10+r*20,10+f*10,4,4);
+  }
 }
+
+
+
+
+  }
